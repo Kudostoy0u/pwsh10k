5e04c72213639850f85f0b9f8300f0ce1d29edbc
Kudos Beluga <kudosbeluga@gmail.com>
2021-01-10 13:31:34 -0600

    made installation script

--- install.ps1
+++ install.ps1
@@ -1,2 +1,7 @@
 # C:\Users\kudos\Documents\PowerShell\Modules\oh-my-posh\2.0.492\Themes\powerlevel.psm1
-Move-Item -Path ./pwsh10k.psm1 -Destination ~/Documents/Power
+$versionpath = "PowerShell"
+if (!((Get-Host | Select-Object Version).Version.Major -eq 7)) {
+$versionpath = "WindowsPowerShell"
+Write-Host "Why aren't you on Powershell 7? This may or may not work."
+}
+Move-Item -Path ./pwsh10k.psm1 -Destination ~/Documents/$versionpath/Modules/oh-my-posh/*/Themes/pwsh10k.psm1
