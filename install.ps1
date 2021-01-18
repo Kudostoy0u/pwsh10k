d55995368c2d684db54b00ccd869778bfb0c9c10
Kudos Beluga <69732000+Kudostoy0u@users.noreply.github.com>
2021-01-18 13:06:07 -0600

    Delete install.ps1

--- install.ps1
+++
@@ -1,9 +0,0 @@
-# C:\Users\kudos\Documents\PowerShell\Modules\oh-my-posh\2.0.492\Themes\powerlevel.psm1
-$versionpath = "PowerShell"
-if (!((Get-Host | Select-Object Version).Version.Major -eq 7)) {
-$versionpath = "WindowsPowerShell"
-Write-Host "Why aren't you on Powershell 7? This may or may not work."
-}
-$nextdir = Get-ChildItem -Path ~\Documents\$versionpath\Modules\oh-my-posh\ -Name
-Copy-Item -Path pwsh10k.psm1 -Destination  ~\Documents\$versionpath\Modules\oh-my-posh\$nextdir\Themes
-# /Modules/oh-my-posh/*/Themes/pwsh10k.psm1
