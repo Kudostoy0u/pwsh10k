8fe2f00559b30e3e6421d5cfa0aa0b95e43a4a7b
Kudos Beluga <69732000+Kudostoy0u@users.noreply.github.com>
2021-12-02 11:41:05 -0600

    Delete install.ps1

--- install.ps1
+++
@@ -1,4 +0,0 @@
-if (!((Get-Host | Select-Object Version).Version.Major -eq 7)) {
-Write-Host "I told you to use PowerShell 7 -_-. Fine, I'll give you a pass."
-}
-Copy-Item -Path pwsh10k.omp.json -Destination $HOME
